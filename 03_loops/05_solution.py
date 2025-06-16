9009f3ccf124390bcea5821fe4494776de170512
Het Kachhadia <Het.kachhdia29@gmail.com>
2025-06-17 00:24:09 +0530

    Delete 03_loops/05_solution.py

--- 03_loops/05_solution.py
+++
@@ -1,8 +0,0 @@
-## 5. Find the First Non-Repeated Character
-
-# Problem: Given a string, find the first non-repeated character.
-
-s = str(input("Enter the string: "))
-l = len(s)+1
-# kachhadia -> k
-# hhappy -> a
