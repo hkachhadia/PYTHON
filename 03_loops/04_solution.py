1a0449fedcb3a988db6f65413e4339c76b2247dd
Het Kachhadia <Het.kachhdia29@gmail.com>
2025-06-18 20:12:03 +0530

    Delete 03_loops/04_solution.py

--- 03_loops/04_solution.py
+++
@@ -1,9 +0,0 @@
-## 4. Reverse a String
-
-## Problem: Reverse a string using a loop.
-
-s = str(input("Enter the string : "))
-# print(x)
-# print(s[::-1])
-for x in range(len(s),0,-1):
-    print(s[x-1], end = '')
